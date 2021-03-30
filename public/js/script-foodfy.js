7600b9ce8ef4d99e04665b9b582519afb5a0f433
Weverson Diazz <wevers.info@gmail.com>
2021-03-30 12:21:16 -0300

    new function
    Função para esconder informação da receita e correção de id

--- public/js/script-foodfy.js
+++ public/js/script-foodfy.js
@@ -55,47 +55,66 @@ function ocultarPreparo() {
         }
  }
 
+ /* --- Nova função mostrar/esconder  --- */
+
+ function hiddenRecipeInformation(button) {     
+    
+    const recipeInformation = document.querySelector(".detalheReceita .add-recipe");
+
+    if (recipeInformation.classList.contains("ocultar")) {
+        
+        recipeInformation.classList.remove("ocultar");
+        button.innerHTML = "esconder";
+    }
+    else {
+        recipeInformation.classList.add("ocultar");
+        button.innerHTML = "mostrar";
+    }
+
+     console.log(recipeInformation);
+ }
+
 
 /* === Create New Recipe === */
 
-function addIngredient() {
-    const ingredients = document.querySelector("#ingredients");
-    const fieldContainer = document.querySelectorAll(".ingredient");
+// function addIngredient() {
+//     const ingredients = document.querySelector("#ingredients");
+//     const fieldContainer = document.querySelectorAll(".ingredient");
 
-    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+//     const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
 
-    if (newField.children[0].value == "") return false;
+//     if (newField.children[0].value == "") return false;
 
-    newField.children[0].value = "";
-    ingredients.appendChild(newField);
-}
+//     newField.children[0].value = "";
+//     ingredients.appendChild(newField);
+// }
 
-document.querySelector(".add-ingredient").addEventListener("click", addIngredient);
+// document.querySelector(".add-ingredient").addEventListener("click", addIngredient);
 
-function addPassosPreparo() {
-const modoPreparo = document.querySelector("#modoPreparo");
-const fieldContainerPreparo = document.querySelectorAll(".passoPreparo");
+// function addPassosPreparo() {
+// const modoPreparo = document.querySelector("#modoPreparo");
+// const fieldContainerPreparo = document.querySelectorAll(".passoPreparo");
 
-const newPasso = fieldContainerPreparo[fieldContainerPreparo.length - 1].cloneNode(true);
+// const newPasso = fieldContainerPreparo[fieldContainerPreparo.length - 1].cloneNode(true);
 
-if (newPasso.children[0].value == "") return false;
+// if (newPasso.children[0].value == "") return false;
 
-newPasso.children[0].value = "";
-modoPreparo.appendChild(newPasso);
-}
+// newPasso.children[0].value = "";
+// modoPreparo.appendChild(newPasso);
+// }
 
-document.querySelector(".add-passo").addEventListener("click", addPassosPreparo);
+// document.querySelector(".add-passo").addEventListener("click", addPassosPreparo);
 
 
 /* === Delete Recipe === */
 
-const formDelete = document.querySelector("#form-delete");
+// const formDelete = document.querySelector("#form-delete");
 
-        formDelete.addEventListener("submit", function(event){
-            const confirmation = confirm("Deseja deletar o cadastro?");
+//         formDelete.addEventListener("submit", function(event){
+//             const confirmation = confirm("Deseja deletar o cadastro?");
 
-            if(!confirmation) {
-                event.preventDefault();
-            }
+//             if(!confirmation) {
+//                 event.preventDefault();
+//             }
             
-        });
+//         });
