00c502d746254a1d7e59c2f9f1a2ab7daa5bf5f6
Weverson Diazz <wevers.info@gmail.com>
2021-10-24 14:35:53 -0300

    Menu ativo refatorando

--- public/js/menuActive.js
+++ public/js/menuActive.js
@@ -2,9 +2,19 @@
 const currentPageFoodfy = window.location.pathname;
 
 const menuLinks = document.querySelectorAll("#header-adm nav.links_adm a");
+const linksVisitors = document.querySelectorAll(".topo-principal nav ul li a");
 
 for (link of menuLinks) {
     if(currentPageFoodfy.includes(link.getAttribute("href"))) {
         link.classList.add("active");        
     }
 }
+
+for (link of linksVisitors) {
+    if(currentPageFoodfy.includes(link.getAttribute("href"))) {
+        link.classList.add("active");        
+    }
+}
+
+console.log("To aqui!")
+console.log(linksVisitors);
